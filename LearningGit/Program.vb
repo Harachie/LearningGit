690c81af27fea14c5af87e94966b70b5752c577d
Bastian Kröger <bkr@efi.com>
2019-02-18 16:09:27 +0100

    Added method to draw hangman word

--- LearningGit/Program.vb
+++ LearningGit/Program.vb
@@ -1,7 +1,21 @@
 ﻿Module Program
 
     Sub Main()
+        Dim wort As String
+        Dim richtigGerateneBuchstaben As String
 
+        wort = "Baum"
+        richtigGerateneBuchstaben = "au"
+    End Sub
+
+    Sub ZeichneHangmanWort(wort As String, richtigGerateneBuchstaben As String)
+        For i As Integer = 0 To wort.Length - 1
+            If richtigGerateneBuchstaben.Contains(wort(i)) Then
+                Console.Write(wort(i))
+            Else
+                Console.Write("_")
+            End If
+        Next
     End Sub
 
 End Module
